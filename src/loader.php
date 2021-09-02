6c5f013b8cced3fe7dc07ae8f03de77789eb3b76
Elbek Khamdullaev <elbek.khamdullaev@mail.ru>
2021-09-02 18:40:43 +0500

    upd

--- src/loader.php
+++ src/loader.php
@@ -1,11 +1,13 @@
 <?php
 
-require 'config/Config.php';
-require 'framework/Framework.php';
-require 'method/Method.php';
-require 'plugin/Plugin.php';
-require 'update/Update.php';
+if (phpversion() >= 7.1) {
+    require 'config/Config.php';
+    require 'framework/Framework.php';
+    require 'method/Method.php';
+    require 'plugin/Plugin.php';
+    require 'update/Update.php';
 
-if (in_array(['sqlite3', 'mysqli', 'pdo'], get_loaded_extensions())) {
-    require 'database/Database.php';
+    if (in_array(['sqlite3', 'mysqli', 'pdo'], get_loaded_extensions())) {
+        require 'database/Database.php';
+    }
 }
